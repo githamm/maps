bb6250169955c55a7ef686de011780d1f2566dbc
githamm <hamm.kevin@gmail.com>
2016-05-18 18:10:22 -0600

    Add farmers market map

---
+++ farmers-markets/farmers_market_json.js
@@ -0,0 +1,264 @@
+var farmersMarkets = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.295868, 40.012629]
+        },
+        "properties": {
+            "name": "Boulder Farmers Market",
+            "address": "13th Street between Canyon Boulevard and Arapahoe Avenue",
+            "city": "Boulder",
+            "zip": 80302,
+            "allTime": "April 2-Nov. 19: Saturdays, 8 am-2 pm; May 4-Oct 5: Wednesdays, 4-8 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.990162, 39.590997]
+        },
+        "properties": {
+            "name": "Broadridge Plaza Farmers Market",
+            "address": "6905 S. Broadway",
+            "city": "Littleton",
+            "zip": 80122,
+            "allTime": "June 16-Oct. 13: Thursdays, 10 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.958053, 39.717998]
+        },
+        "properties": {
+            "name": "Cherry Creek Fresh Market",
+            "address": "1st Avenue and University Boulevard",
+            "city": "Denver",
+            "zip": 80209,
+            "allTime": "May 7-Oct. 29: Saturdays, 8 am-1 pm; June 15-Sept. 28: Wednesdays, 9 am-1 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.957402, 39.740184]
+        },
+        "properties": {
+            "name": "City Park Esplanade Fresh Market",
+            "address": "East Colfax Avenue and Columbine Street",
+            "city": "Denver",
+            "zip": 80205,
+            "allTime": "June 5-Oct. 30: Sundays, 9 am-1 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.075175, 39.762117]
+        },
+        "properties": {
+            "name": "Four Seasons Farmers &amp; Artisans Market",
+            "address": "7043 W. 32nd Ave.",
+            "city": "Wheat Ridge.",
+            "zip": 80033,
+            "allTime": "June 4-Oct. 31: Saturdays, 9 am-1 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.900118, 39.617332]
+        },
+        "properties": {
+            "name": "Fresh Market Pop-up at the Landmark",
+            "address": "7600 Landmark Way",
+            "city": "Greenwood Village",
+            "zip": 80111,
+            "allTime": "July 6-Aug. 31: Wednesdays, 4-8 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.891438, 39.787018]
+        },
+        "properties": {
+            "name": "Fresh Market Pop-Up at Stapleton",
+            "address": "Conservatory Green at Stapleton: East 49th Place and Valentia Court",
+            "city": "Denver",
+            "zip": 80238,
+            "allTime": "Aug. 11-Sept. 1: Thursdays, 5-8 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.225554, 39.755914]
+        },
+        "properties": {
+            "name": "Golden Farmers Market",
+            "address": "1019 10th St. ",
+            "city": "Golden",
+            "zip": 80401,
+            "allTime": "June 4-Oct. 1: Saturdays, 8 am-1 pm (no market July 30)"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.999796, 39.547692]
+        },
+        "properties": {
+            "name": "Highlands Ranch Farmers Market",
+            "address": " 9288 Dorchester St.",
+            "city": "Highlands Ranch",
+            "zip": 80126,
+            "allTime": "May 8-Oct. 30: Sundays, 10 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.098467, 39.711109]
+        },
+        "properties": {
+            "name": "Lakewood Farmers Market",
+            "address": "9077 W. Alameda Ave.",
+            "city": "Lakewood",
+            "zip": 80226,
+            "allTime": "June 18-Oct. 1: Saturdays, 10 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.0795, 40.586378]
+        },
+        "properties": {
+            "name": "Larimer County Farmers Market",
+            "address": "200 W. Oak St.",
+            "city": "Fort Collins",
+            "zip": 80524,
+            "allTime": "May 14-Oct. 22: Saturdays, 8 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.025323, 39.584483]
+        },
+        "properties": {
+            "name": "Littleton Farmers Market",
+            "address": "7301 S. Santa Fe Drive",
+            "city": "Littleton",
+            "zip": 80120,
+            "allTime": "June 15-Oct. 12: Wednesdays, 10 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.12567, 40.153145]
+        },
+        "properties": {
+            "name": "Longmont Farmers Market at Fairgrounds Park",
+            "address": " 9595 Nelson Road",
+            "city": "Longmont",
+            "zip": 80501,
+            "allTime": "April-Nov. 19: Saturdays, 8 am-1 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.07498, 40.397761]
+        },
+        "properties": {
+            "name": "Loveland Farmers Market",
+            "address": "700 S. Railroad Ave, Pavilions No. 1 and No. 2",
+            "city": "Loveland",
+            "zip": 80537,
+            "allTime": "June 26-Sept. 25: Sundays, 9 am-1:30 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.980499, 39.682169]
+        },
+        "properties": {
+            "name": "South Pearl Street Farmers Market",
+            "address": "1500 block of Old South Pearl Street",
+            "city": "Denver",
+            "zip": 80210,
+            "allTime": "May 15-Nov. 13: Sundays, 9 am-1 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.095775, 39.612122]
+        },
+        "properties": {
+            "name": "Southwest Plaza Farmers Market",
+            "address": "8501 W. Bowles Ave.",
+            "city": "Littleton",
+            "zip": 80123,
+            "allTime": "May 7-Oct. 29: Saturdays, 8 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.900525, 39.757702]
+        },
+        "properties": {
+            "name": "Stapleton Fresh Market",
+            "address": "East 29th Avenue and Roslyn Street",
+            "city": "Denver",
+            "zip": 80238,
+            "allTime": "June 19-Oct. 16: Sundays, 8:30 am-12:30 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.000145, 39.753122]
+        },
+        "properties": {
+            "name": "Union Station Farmers Market",
+            "address": "1701 Wynkoop St.",
+            "city": "Denver",
+            "zip": 80202,
+            "allTime": "June 4-Oct. 22: Saturdays, 9 am-2 pm"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.688619, 40.4236]
+        },
+        "properties": {
+            "name": "Greeley Farmers Market",
+            "address": "902 7th Ave.",
+            "city": "Greeley",
+            "zip": 80631,
+            "allTime": "May 21-Oct. 29: Saturdays, 7:30 am-noon"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-105.155653, 40.171524]
+        },
+        "properties": {
+            "name": "Zweck's Fresh Vegetables & Flowers",
+            "address": "10901 Airport Road",
+            "city": "Longmont",
+            "zip": 80503,
+            "allTime": "July 15-Oct. 8: Mondays, 3-5 pm; Tuesdays-Fridays, 10 am-1 pm and 3-5:30 pm; Sundays, 3-5:30 pm"
+        }
+    }]
+}
