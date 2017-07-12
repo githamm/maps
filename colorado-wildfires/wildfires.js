2df2da13d4688b8c1bc2b4e88451007083633ebf
githamm <khamm@denverpost.com>
2017-07-12 17:42:43 -0600

    Update styles from R > Leaflet

--- colorado-wildfires/wildfires.js
+++ colorado-wildfires/wildfires.js
@@ -5,17 +5,48 @@
     "type": "Feature",
     "geometry": {
        "type": "Point",
-       "coordinates":  [ -106.087,39.532 ]
+       "coordinates":  [ -108.026,37.598 ]
     },
     "properties": {
-    "fireName":"Peak 2 Fire",
-    "fireCause":"",
-    "fireStart":"7/5/2017",
+    "fireName":"412 Fire",
+    "fireCause":"Lightning",
+    "fireStart":"6/23/2017",
     "fireStatus":"Active",
-    "fireAcres":85,
-    "pctContained":7,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5321/",
-    "fireDetails":""
+    "fireAcres":95,
+    "pctContained":null,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5286/"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -108.815,40.153 ]
+    },
+    "properties": {
+    "fireName":"Dead Dog Fire",
+    "fireCause":"Human",
+    "fireStart":"6/11/2017",
+    "fireStatus":"Inactive",
+    "fireAcres":17731,
+    "pctContained":100,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5242/"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -108.005,39.749 ]
+    },
+    "properties": {
+    "fireName":"Deer Gulch Fire",
+    "fireCause":"Unknown",
+    "fireStart":"7/8/2017",
+    "fireStatus":"Active",
+    "fireAcres":43,
+    "pctContained":50,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5341/"
     }
   },
   {
@@ -29,10 +60,9 @@
     "fireCause":"Lightning",
     "fireStart":"7/2/2017",
     "fireStatus":"Active",
-    "fireAcres":689,
-    "pctContained":8,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5310/",
-    "fireDetails":""
+    "fireAcres":700,
+    "pctContained":40,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5310/"
     }
   },
   {
@@ -43,13 +73,28 @@
     },
     "properties": {
     "fireName":"Gutzler Fire",
-    "fireCause":"",
+    "fireCause":"Unknown",
     "fireStart":"7/2/2017",
     "fireStatus":"Active",
-    "fireAcres":1000,
-    "pctContained":null,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5305/",
-    "fireDetails":""
+    "fireAcres":974,
+    "pctContained":20,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5305/"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -108.312,39.798 ]
+    },
+    "properties": {
+    "fireName":"Hunter Fire",
+    "fireCause":"Lightning",
+    "fireStart":"6/10/2017",
+    "fireStatus":"Inactive",
+    "fireAcres":992,
+    "pctContained":100,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5240/"
     }
   },
   {
@@ -65,76 +110,23 @@
     "fireStatus":"Active",
     "fireAcres":412,
     "pctContained":95,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5298/",
-    "fireDetails":""
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5298/"
     }
   },
   {
     "type": "Feature",
     "geometry": {
        "type": "Point",
-       "coordinates":  [ -108.026,37.598 ]
+       "coordinates":  [ -107.141,40.67 ]
     },
     "properties": {
-    "fireName":"412 Fire",
-    "fireCause":"Lightning",
-    "fireStart":"6/23/2017",
-    "fireStatus":"Active",
-    "fireAcres":95,
-    "pctContained":10,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5286/",
-    "fireDetails":""
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-       "type": "Point",
-       "coordinates":  [ -108.815,40.153 ]
-    },
-    "properties": {
-    "fireName":"Dead Dog Fire",
+    "fireName":"Mill Creek Fire",
     "fireCause":"Human",
-    "fireStart":"6/11/2017",
+    "fireStart":"7/1/2017",
     "fireStatus":"Active",
-    "fireAcres":17731,
-    "pctContained":100,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5242/",
-    "fireDetails":""
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-       "type": "Point",
-       "coordinates":  [ -108.312,39.798 ]
-    },
-    "properties": {
-    "fireName":"Hunter Fire",
-    "fireCause":"Lightning",
-    "fireStart":"6/10/2017",
-    "fireStatus":"Active",
-    "fireAcres":992,
-    "pctContained":100,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5240/",
-    "fireDetails":""
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-       "type": "Point",
-       "coordinates":  [ -108.834,40.794 ]
-    },
-    "properties": {
-    "fireName":"Peekaboo Fire",
-    "fireCause":"Lightning",
-    "fireStart":"6/27/2017",
-    "fireStatus":"Active",
-    "fireAcres":8000,
-    "pctContained":5,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5328/",
-    "fireDetails":""
+    "fireAcres":482,
+    "pctContained":72,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5330/"
     }
   },
   {
@@ -150,25 +142,55 @@
     "fireStatus":"Inactive",
     "fireAcres":38,
     "pctContained":100,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5249/",
-    "fireDetails":""
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5249/"
     }
   },
   {
     "type": "Feature",
     "geometry": {
        "type": "Point",
-       "coordinates":  [ -107.141,40.67 ]
+       "coordinates":  [ -106.087,39.532 ]
     },
     "properties": {
-    "fireName":"Mill Creek Fire",
+    "fireName":"Peak 2 Fire",
     "fireCause":"Human",
-    "fireStart":"7/1/2017",
+    "fireStart":"7/5/2017",
     "fireStatus":"Active",
-    "fireAcres":460,
-    "pctContained":30,
-    "inciwebLink":"https://inciweb.nwcg.gov/incident/5330/",
-    "fireDetails":""
+    "fireAcres":84,
+    "pctContained":85,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5321/"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -108.834,40.794 ]
+    },
+    "properties": {
+    "fireName":"Peekaboo Fire",
+    "fireCause":"Lightning",
+    "fireStart":"6/27/2017",
+    "fireStatus":"Active",
+    "fireAcres":12839,
+    "pctContained":68,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5328/"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -107.919,40.152 ]
+    },
+    "properties": {
+    "fireName":"Wilson Fire",
+    "fireCause":"Lightning",
+    "fireStart":"7/7/2017",
+    "fireStatus":"Active",
+    "fireAcres":350,
+    "pctContained":25,
+    "inciwebLink":"https://inciweb.nwcg.gov/incident/5338/"
     }
   }
 ]
